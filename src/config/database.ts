f6a49116aba0349b1c182dcd8d83593e4d65cb58
Gianmarco Chistama <gianmarcovalentinc@gmail.com>
2024-12-04 23:00:37 -0500

    model: crear modelo para productos

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,11 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import colors from 'colors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const database = new Sequelize(process.env.DATABASE_URL);
+const database = new Sequelize(process.env.DATABASE_URL!, {
+	models: [__dirname + '/../models/**/*.ts'],
+});
 
 export async function connectDatabase() {
 	try {
