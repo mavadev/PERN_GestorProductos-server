42a244cac042856ddb20fba43861ac90c9af3403
Gianmarco Chistama <gianmarcovalentinc@gmail.com>
2024-12-08 01:30:15 -0500

    test[config]: code coverage - metrica de validacion

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,112 +2,84 @@ import { Request, Response } from 'express';
 import Product from '../models/Product.model';
 
 export const getProductById = async (req: Request, res: Response) => {
-	try {
-		const { id } = req.params;
-		const product = await Product.findByPk(id);
+	const { id } = req.params;
+	const product = await Product.findByPk(id);
 
-		if (!product) {
-			res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
-			return;
-		}
-
-		res.json({ data: product });
-	} catch (error) {
-		console.error(error);
+	if (!product) {
+		res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
+		return;
 	}
+
+	res.json({ data: product });
 };
 
 export const getProducts = async (req: Request, res: Response) => {
-	try {
-		const products = await Product.findAll({
-			order: [['id', 'ASC']],
-			attributes: {
-				exclude: ['createdAt', 'updatedAt'],
-			},
-		});
-		res.json({ data: products });
-	} catch (error) {
-		console.error(error);
-	}
+	const products = await Product.findAll({
+		order: [['id', 'ASC']],
+		attributes: {
+			exclude: ['createdAt', 'updatedAt'],
+		},
+	});
+	res.json({ data: products });
 };
 
 export const createProduct = async (req: Request, res: Response) => {
-	try {
-		const product = await Product.create(req.body);
-		res.status(201).json({ data: product });
-	} catch (error) {
-		console.error(error);
-	}
+	const product = await Product.create(req.body);
+	res.status(201).json({ data: product });
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-	try {
-		// Obtener producto a actualizar
-		const { id } = req.params;
-		const product = await Product.findByPk(id);
-		// Validación
-		if (!product) {
-			res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
-			return;
-		}
-		// Actualizar Producto
-		await product.update(req.body);
-		await product.save();
-
-		res.json({ data: product });
-	} catch (error) {
-		console.error(error);
+	// Obtener producto a actualizar
+	const { id } = req.params;
+	const product = await Product.findByPk(id);
+	// Validación
+	if (!product) {
+		res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
+		return;
 	}
+	// Actualizar Producto
+	await product.update(req.body);
+	await product.save();
+
+	res.json({ data: product });
 };
 
 export const updateAvailability = async (req: Request, res: Response) => {
-	try {
-		const { id } = req.params;
-		const product = await Product.findByPk(id);
+	const { id } = req.params;
+	const product = await Product.findByPk(id);
 
-		if (!product) {
-			res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
-			return;
-		}
-
-		product.availability = !product.dataValues.availability;
-		await product.save();
-
-		res.json({ data: product });
-	} catch (error) {
-		console.error(error);
+	if (!product) {
+		res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
+		return;
 	}
+
+	product.availability = !product.dataValues.availability;
+	await product.save();
+
+	res.json({ data: product });
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
-	try {
-		const { id } = req.params;
-		const product = await Product.findByPk(id);
+	const { id } = req.params;
+	const product = await Product.findByPk(id);
 
-		if (!product) {
-			res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
-			return;
-		}
-
-		await product.destroy();
-		await product.save();
-
-		res.json({ data: `Producto eliminado (ID: ${id})` });
-	} catch (error) {
-		console.error(error);
+	if (!product) {
+		res.status(404).json({ error: `Producto no encontrado (ID: ${id})` });
+		return;
 	}
+
+	await product.destroy();
+	await product.save();
+
+	res.json({ data: `Producto eliminado (ID: ${id})` });
 };
 
 export const deleteProducts = async (req: Request, res: Response) => {
-	try {
-		const count = await Product.count();
-		await Product.destroy({
-			truncate: true,
-			restartIdentity: true,
-		});
+	const count = await Product.count();
+	await Product.destroy({
+		truncate: true,
+		restartIdentity: true,
+	});
 
-		res.json({ data: `Productos eliminados (${count})` });
-	} catch (error) {
-		console.error(error);
-	}
+	res.json({ data: `Productos eliminados (${count})` });
 };
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
 
 export const validateErrors = (req: Request, res: Response, next: NextFunction) => {
-	// Validacion
+	// Validación
 	let errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		res.status(400).json({ errors: errors.array() });
