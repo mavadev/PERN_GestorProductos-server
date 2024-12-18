e205437ce7dafbd7f2269e07da59294f8591c94d
Gianmarco Chistama <gianmarcovalentinc@gmail.com>
2024-12-18 13:01:11 -0500

    config: habilitar la escucha en puerto y direccion IP (responsive en cliente)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import colors from 'colors';
 import server, { connectDatabase } from './server';
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+server.listen(PORT, HOST, () => {
 	connectDatabase();
 	console.log(colors.cyan.bold(`Server activo en puerto ${PORT}`));
 });
